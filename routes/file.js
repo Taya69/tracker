14c3dbb37a32ba37fd3ef43f588c3e8cef9c8b95
Taya69 <27taya@mail.ru>
2021-07-30 14:20:26 +0300

    second

--- routes/file.js
+++ routes/file.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controllerTasks = require('../controllers/tasks')
 
-
 router.post('/', controllerTasks.addFile)
 router.get('/', controllerTasks.download)
 
