5f3c78b9946a20c7b8381b1c15708da9aa82850f
Taya69 <27taya@mail.ru>
2021-07-30 14:38:27 +0300

    fix router

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor (private _router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       if (localStorage.getItem('token') !== null) {        
-        return true} else {          
+        return true} else {  
+          this._router.navigate([''])        
         return false;
       }   
-  }
-  
+  }  
 }
